6ee6e823f9e0af7b255c5a70f8796f926d99e835
rzeznik-kasia <rzeznik.kasia@gmail.com>
2017-08-30 16:20:36 +0200

    componenty laduja sie do komputera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './components/items/items.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    RegisterFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'items', component: ItemsComponent},
+      {path: 'register-form', component: RegisterFormComponent}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
