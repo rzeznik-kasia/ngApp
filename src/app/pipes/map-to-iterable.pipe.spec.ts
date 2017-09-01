6bb16f91e52f2984b4e26960f489831b861dd678
rzeznik-kasia <rzeznik.kasia@gmail.com>
2017-09-01 12:18:15 +0200

    testy iinit

--- src/app/pipes/map-to-iterable.pipe.spec.ts
+++ src/app/pipes/map-to-iterable.pipe.spec.ts
@@ -5,4 +5,10 @@ describe('MapToIterablePipe', () => {
     const pipe = new MapToIterablePipe();
     expect(pipe).toBeTruthy();
   });
+  it('should return array', ()=> {
+    const pipe = new MapToIterablePipe();
+    const result = pipe.transform({id:1});
+    console.log(result);
+    expect(result).toEqual(['id']); // to be sprawdza czy to jest tym obiektem, a eqals sprawdza czy to
+  })
 });
