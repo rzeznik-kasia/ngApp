8710fce4dd5bd29d7a1d65391cfa6c15ad267767
rzeznik-kasia <rzeznik.kasia@gmail.com>
2017-09-01 12:29:24 +0200

    testy iinit

--- src/app/components/datagrid/datagrid.component.spec.ts
+++ src/app/components/datagrid/datagrid.component.spec.ts
@@ -1,25 +1,32 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {DatagridComponent} from "./datagrid.component";
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
-import { DatagridComponent } from './datagrid.component';
+describe('datagrid component', ()=> {
+  /*musimy stworzyc modul ktory zawiera cos co testujemy*/
 
-describe('DatagridComponent', () => {
-  let component: DatagridComponent;
-  let fixture: ComponentFixture<DatagridComponent>;
+  let fixture: ComponentFixture<DatagridComponent>; /*opakowany komponent w narzedzia do testowania*/
+  let template: DebugElement /*to co jest w klasie - ten model*/
+  let instance: DatagridComponent; /*instancja tego co chemy testowac*/
 
-  beforeEach(async(() => {
+  beforeEach(()=> {
     TestBed.configureTestingModule({
-      declarations: [ DatagridComponent ]
+      declarations: [
+        DatagridComponent //testujemy datagrid compontnet
+      ]
     })
-    .compileComponents();
-  }));
+  })
 
-  beforeEach(() => {
+  it('should create instance', ()=> {
     fixture = TestBed.createComponent(DatagridComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    template = fixture.debugElement; //zobaczymy co w tej templatce jest, tbody jest pusciutkie - tak powinno byc
+
+    expect(fixture).toBeTruthy();
+    console.log(template);
+    const rows = template.queryAll(By.css('table tbody tr'))
+    console.log(rows);
+
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
-});
+})
--- src/test.ts
+++ src/test.ts
@@ -25,7 +25,7 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /map-to-iterable.pipe.spec\.ts$/);
+const context = require.context('./', true, /datagrid.component.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
 // Finally, start Karma to run the tests.
