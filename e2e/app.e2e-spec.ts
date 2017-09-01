80653e2f72a7898e4b5760f1217e6199997c6f92
rzeznik-kasia <rzeznik.kasia@gmail.com>
2017-09-01 11:00:20 +0200

    testy iinit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -3,15 +3,28 @@ import {browser, By, element} from "protractor";
 describe('items view', ()=> {
 
   const btnLogIn = element(By.buttonText('log in'))
+  const datagrid = element(By.tagName('app-datagrid')); //albo by css
+  const search = element(By.tagName('app-search'));
+  const btnAddItem = element(By.buttonText('add'));
 
   beforeAll(()=> {
     browser.get('items')
     btnLogIn.click();
-    expect(btnLogIn.isPresent()).toBeFalsy();
+    expect(btnLogIn.isPresent()).toBeFalsy(); /*sprawdza czy zniknal przycisk*/
   })
 
   it('should open items view', ()=> {
     browser.get('items');
-    browser.sleep(5000);
+    browser.sleep(5000); /*tu nic nie sprawdzamy */
   });
+
+  it('should display elements', ()=> {
+    browser.get('items');
+    expect(datagrid.isPresent()).toBeTruthy();
+    expect(search.isPresent()).toBeTruthy();
+    expect(btnAddItem.isPresent()).toBeTruthy(); //normalnie kazdy z tych elementow powinien byc w osobnym itcie
+  });
+
+
+
 });
