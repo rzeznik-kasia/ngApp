02aa839814bc96ac8e78183cff1733e4e9e109ac
rzeznik-kasia <rzeznik.kasia@gmail.com>
2017-09-01 13:45:12 +0200

    testy iinit

--- src/app/components/datagrid/datagrid.component.spec.ts
+++ src/app/components/datagrid/datagrid.component.spec.ts
@@ -8,7 +8,7 @@ describe('datagrid component', ()=> {
 
   let fixture: ComponentFixture<DatagridComponent>; /*opakowany komponent w narzedzia do testowania*/
   let template: DebugElement /*to co jest w klasie - ten model*/
-  let instance: DatagridComponent; /*instancja tego co chemy testowac*/
+  let instance: DatagridComponent; /*instancja tego co chemy testowac, np. new DataGrid*/
 
   beforeEach(()=> {
     TestBed.configureTestingModule({
@@ -18,14 +18,46 @@ describe('datagrid component', ()=> {
     })
   })
 
-  it('should create instance', ()=> {
+  it('should create instance with 0 row', ()=> {
     fixture = TestBed.createComponent(DatagridComponent);
-    template = fixture.debugElement; //zobaczymy co w tej templatce jest, tbody jest pusciutkie - tak powinno byc
+    template = fixture.debugElement; //zobaczymy co w tej templatce jest, tbody jest pusciutkie - tak powinno byc // templatka umozliwiajaca np. wyszukiwanie
 
     expect(fixture).toBeTruthy();
     console.log(template);
     const rows = template.queryAll(By.css('table tbody tr'))
-    console.log(rows);
+    console.log(rows); //widac ze jest pusta tablica
+
+    expect(rows.length).toBe(0);
+  });
+
+  it('should create instance with 2 rows', ()=> {
+    fixture = TestBed.createComponent(DatagridComponent);
+    template = fixture.debugElement; //zobaczymy co w tej templatce jest, tbody jest pusciutkie - tak powinno byc // templatka umozliwiajaca np. wyszukiwanie
+    instance = fixture.componentInstance;
+    instance.items = [{name: "joe"}, {name: "mike"}]
+
+    console.log(template);
+
+    instance.config = ['name'];
+
+    fixture.detectChanges();
+    const rows = template.queryAll(By.css('table tbody tr'));
+    console.log('rows' + rows.length); //widac ze jest pusta tablica
+
+    //expect(rows.length).toBe(0); to na chwile zakomentowac bo nie przejdzie
+    //sprawdzenie ze elementow jest 2
+    expect(rows.length).toBe(2);
+    //expect(rows[0].getText());
+    //console.log('..', );
+    const el1 = rows[0].nativeElement.innerHTML;
+    expect(el1).toContain('joe');
+
+
+    const el1Body = rows[0];
+    console.log(el1Body.queryAll(By.css('td')));
+    //
+
+
 
   });
 
